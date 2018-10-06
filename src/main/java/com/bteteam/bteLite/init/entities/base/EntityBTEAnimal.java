2b274d510a0e68586b86ead319af438ac466dfbb
Tebreca <ewan.arends10@gmail.com>
2018-10-06 07:16:17 +0200

    add textures and models I had in the cloud

---
+++ src/main/java/com/bteteam/bteLite/init/entities/base/EntityBTEAnimal.java
@@ -0,0 +1,14 @@
+package com.bteteam.bteLite.init.entities.base;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+
+public abstract class EntityBTEAnimal extends EntityLivingBase{
+
+	public EntityBTEAnimal(World worldIn) {
+		super(worldIn);
+	}
+	
+	
+
+}
